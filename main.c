550ec267d9203b5232949b208ee763e9e514bbb2
takase1121 <20792268+takase1121@users.noreply.github.com>
2022-08-13 12:26:38 +0800

    add license

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+/**
+ * This file is licensed under 0-BSD.
+ * License text is attached after source code.
+ */
 #define _XOPEN_SOURCE 600
 
 #include <unistd.h>
@@ -237,3 +241,20 @@ failure:
     close(mode_fd);
   return rc;
 }
+
+/**
+ * BSD Zero Clause License
+ * 
+ * Copyright (c) 2022 takase1121
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
